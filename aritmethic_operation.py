abc4667e8c43ffc7428101d8f1c05f15fa8835fc
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2019-10-04 09:58:14 +0700

    add new file arithmetic_operation.py

---
+++ aritmethic_operation.py
@@ -0,0 +1,14 @@
+firstNumber = 20
+secondNumber = 5
+
+# The output : 20 + 5 = 25
+print('20 + 5 = ', firstNumber+secondNumber)
+
+# The output : 20 - 5 = 15
+print('20 - 5 = ', firstNumber-secondNumber)
+
+# The output : 20 x 5 = 100
+print('20 * 5 = ', firstNumber*secondNumber)
+
+# The output : 20 / 5 = 4
+print('20 / 5 = ', firstNumber/secondNumber)
