76a333319f190f14e2ce00a791c26627dae11986
GabrielMaiaB <56695772+GabrielMaiaB@users.noreply.github.com>
2019-10-17 15:28:24 -0300

    Update and rename exponentation.py to exponentationAndSquareRoot.py

--- exponentationAndSquareRoot.py
+++ exponentationAndSquareRoot.py
@@ -5,3 +5,12 @@ exp = float(input('What is the exponent? '))
 res = num ** exp
 
 print (res)
+
+#Function for square root
+num2 = float(input('What is the base number? '))
+
+res2 = num2 ** 0.5
+
+print (res2)
+
+
