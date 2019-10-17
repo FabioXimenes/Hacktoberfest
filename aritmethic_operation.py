6e34fbf7de1be717aa2b6b67299888a853960cdb
GabrielMaiaB <56695772+GabrielMaiaB@users.noreply.github.com>
2019-10-17 15:24:08 -0300

    Update aritmethic_operation.py

--- aritmethic_operation.py
+++ aritmethic_operation.py
@@ -1,17 +1,19 @@
 firstNumber = 20
 secondNumber = 5
+thirdNumber = 2
 
 print('First Number: {}'.format(firstNumber))
 print('Second Number: {}'.format(secondNumber))
+print('Third Number: {}'.format(thirdNumber))
 
 # The output : 20 + 5 = 25
-print('20 + 5 = ', firstNumber+secondNumber)
+print('20 + 5 + 2 = ', firstNumber+secondNumber+thirdNumber)
 
 # The output : 20 - 5 = 15
-print('20 - 5 = ', firstNumber-secondNumber)
+print('20 - 5 - 2 = ', firstNumber-secondNumber-thirdNumber)
 
 # The output : 20 x 5 = 100
-print('20 * 5 = ', firstNumber*secondNumber)
+print('20 * 5 * 2 = ', firstNumber*secondNumber*thirdNumber)
 
 # The output : 20 / 5 = 4
-print('20 / 5 = ', firstNumber/secondNumber)
+print('(20 / 5) / 2 = ', firstNumber/secondNumber/thirdNumber)
