337a2ae4f237947aea3db7793597035bea0ab567
Larissa Queiroz <53095319+larisqueiroz@users.noreply.github.com>
2020-10-01 14:33:42 -0300

    update grayscaleimage.py

--- PythonFiles/grayscaleimage.py
+++ PythonFiles/grayscaleimage.py
@@ -1,12 +1,10 @@
-# Abrir uma imagem colorida, transformar em níveis de cinza, visualizar e salvar imagem gerada.
-
 import cv2
 
-image = cv2.imread("image3.jpg")
+original_image = cv2.imread("image3.jpg")
 
-grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+grayscale_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY) 
 
 cv2.imshow("grayscale_image", grayscale_image)
 cv2.waitKey(0)
 
-cv2.imwrite("saved_gray.jpg", grayscale_image)
+cv2.imwrite("saved_gray.jpg", grayscale_image) # saving grayscale image
